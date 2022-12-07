5e68914cb7afde97aa21e81507becde463029f3b
rjax <rjaxware@gmail.com>
2022-12-08 01:23:59 +0200

    Added an algorithm that works with integers

--- 2-6/program.c
+++ 2-6/program.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int setbits(unsigned x, int p, int n, unsigned y);
-int setbits1(int x, int p, int n, int y);
+int setbits2(int x, int p, int n, int y);
 int getRight(unsigned arg, unsigned count);
 int getLeft(unsigned arg, unsigned count);
 
@@ -9,31 +9,36 @@ int main()
 {
     unsigned arg1, arg2, expected, actual;
 
-    arg1 = __UINT32_MAX__;
+    arg1 = __INT32_MAX__;
     arg2 = 0;
     expected = 0;
-    // getRight(arg1, 20);
-    // getLeft(arg1, 20);
-    // printf("\n");
     actual = setbits(arg1, 1, 32, arg2);
+    printf("%s\n", actual == expected ? "OK" : "Fail");
+    actual = setbits2(arg1, 1, 32, arg2);
     printf("%s\n", actual == expected ? "OK" : "Fail");
 
     arg1 = 0;
-    arg2 = __UINT32_MAX__;
-    expected = __UINT32_MAX__;
+    arg2 = __INT32_MAX__;
+    expected = __INT32_MAX__;
     actual = setbits(arg1, 1, 32, arg2);
+    printf("%s\n", actual == expected ? "OK" : "Fail");
+    actual = setbits2(arg1, 1, 32, arg2);
     printf("%s\n", actual == expected ? "OK" : "Fail");
 
     arg1 = 2222;
-    arg2 = __UINT32_MAX__;
-    expected = __UINT32_MAX__;
+    arg2 = __INT32_MAX__;
+    expected = __INT32_MAX__;
     actual = setbits(arg1, 1, 32, arg2);
+    printf("%s\n", actual == expected ? "OK" : "Fail");
+    actual = setbits2(arg1, 1, 32, arg2);
     printf("%s\n", actual == expected ? "OK" : "Fail");
 
     arg1 = 2222;
     arg2 = 0;
     expected = 0;
     actual = setbits(arg1, 1, 32, arg2);
+    printf("%s\n", actual == expected ? "OK" : "Fail");
+    actual = setbits2(arg1, 1, 32, arg2);
     printf("%s\n", actual == expected ? "OK" : "Fail");
 
     arg1 = 0;
@@ -41,11 +46,68 @@ int main()
     expected = 2222;
     actual = setbits(arg1, 1, 32, arg2);
     printf("%s\n", actual == expected ? "OK" : "Fail");
+    actual = setbits2(arg1, 1, 32, arg2);
+    printf("%s\n", actual == expected ? "OK" : "Fail");
 
     arg1 = 2222;
     arg2 = 255;
     expected = 63662;
     actual = setbits(arg1, 13, 4, arg2);
+    printf("%s\n", actual == expected ? "OK" : "Fail");
+    actual = setbits2(arg1, 13, 4, arg2);
+    printf("%s\n", actual == expected ? "OK" : "Fail");
+}
+
+int main1()
+{
+    unsigned arg1, arg2, expected, actual;
+
+    arg1 = __UINT32_MAX__;
+    arg2 = 0;
+    expected = 0;
+    actual = setbits(arg1, 1, 32, arg2);
+    printf("%s\n", actual == expected ? "OK" : "Fail");
+    actual = setbits2(arg1, 1, 32, arg2);
+    printf("%s\n", actual == expected ? "OK" : "Fail");
+
+    arg1 = 0;
+    arg2 = __UINT32_MAX__;
+    expected = __UINT32_MAX__;
+    actual = setbits(arg1, 1, 32, arg2);
+    printf("%s\n", actual == expected ? "OK" : "Fail");
+    actual = setbits2(arg1, 1, 32, arg2);
+    printf("%s\n", actual == expected ? "OK" : "Fail");
+
+    arg1 = 2222;
+    arg2 = __UINT32_MAX__;
+    expected = __UINT32_MAX__;
+    actual = setbits(arg1, 1, 32, arg2);
+    printf("%s\n", actual == expected ? "OK" : "Fail");
+    actual = setbits2(arg1, 1, 32, arg2);
+    printf("%s\n", actual == expected ? "OK" : "Fail");
+
+    arg1 = 2222;
+    arg2 = 0;
+    expected = 0;
+    actual = setbits(arg1, 1, 32, arg2);
+    printf("%s\n", actual == expected ? "OK" : "Fail");
+    actual = setbits2(arg1, 1, 32, arg2);
+    printf("%s\n", actual == expected ? "OK" : "Fail");
+
+    arg1 = 0;
+    arg2 = 2222;
+    expected = 2222;
+    actual = setbits(arg1, 1, 32, arg2);
+    printf("%s\n", actual == expected ? "OK" : "Fail");
+    actual = setbits2(arg1, 1, 32, arg2);
+    printf("%s\n", actual == expected ? "OK" : "Fail");
+
+    arg1 = 2222;
+    arg2 = 255;
+    expected = 63662;
+    actual = setbits(arg1, 13, 4, arg2);
+    printf("%s\n", actual == expected ? "OK" : "Fail");
+    actual = setbits2(arg1, 13, 4, arg2);
     printf("%s\n", actual == expected ? "OK" : "Fail");
 }
 
@@ -80,6 +142,43 @@ int setbits(unsigned x, int p, int n, unsigned y)
     left = left >> 32 - n + 1 - p;
     printBitmap(left, 1);
     unsigned long result = right |= left;
+    printBitmap(result, 1);
+
+    return result;
+}
+
+int setbits2(int x, int p, int n, int y)
+{
+    printf("x: %d, p: %d, n: %d, y: %d\n", x, p, n, y);
+    printBitmap(x, 1);
+    printBitmap(y, 1);
+    printf("\n");
+
+    int i = 0;
+    int num = p > 1 ? 1 : 0;
+    int rightMask = p > 1 ? 1 : 0;
+    for (i = 1; i < p; i++)
+    {
+        rightMask += num *= 2;
+    }
+
+    int right = x & rightMask;
+    printBitmap(right, 1);
+
+    num = n > 0 ? 1 : 0;
+    int leftMask = n > 0 ? 1 : 0;
+    for (i = 1; i < n; i++)
+    {
+        leftMask += num *= 2;
+    }
+
+    printBitmap(leftMask, 1);
+    int left = y & leftMask;
+    printBitmap(left, 1);
+    left <<= p - 1;
+    printBitmap(left, 1);
+
+    int result = right | left;
     printBitmap(result, 1);
 
     return result;
